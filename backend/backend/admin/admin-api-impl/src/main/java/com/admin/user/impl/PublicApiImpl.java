ca298a8b1f086a1b3d967dbd5f797572f96908f6
wz6162659 <470382668@qq.com>
2018-10-24 17:23:43 +0800

    修改实例id

--- backend/backend/admin/admin-api-impl/src/main/java/com/admin/user/impl/PublicApiImpl.java
+++ backend/backend/admin/admin-api-impl/src/main/java/com/admin/user/impl/PublicApiImpl.java
@@ -6,25 +6,31 @@ import com.admin.user.repository.base.service.PublicService;
 import com.hengyunsoft.security.auth.client.annotation.IgnoreToken;
 import com.hengyunsoft.utils.JSONUtils;
 import io.swagger.annotations.Api;
+import lombok.experimental.var;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.UUID;
 
 @Api(value = "PublicApi", description = "外部Api管理")
 @RestController
@@ -40,6 +46,10 @@ public class PublicApiImpl implements PublicApi {
 
     @Autowired
     PublicService publicService;
+
+    private static final String appId = "wx02e940e83a91dcc9";
+    private static final String secret = "027cfdedb223f01f54b9b88c3f0033c4";
+    private static final String grant_type = "client_credential";
 
     @Override
     @IgnoreToken
@@ -63,18 +73,45 @@ public class PublicApiImpl implements PublicApi {
         return dto;
     }
 
+//    @Override
+//    @RequestMapping(value = "/getBrushTicket", method = RequestMethod.GET)
+//    public Object getBrushTicket() throws MalformedURLException {
+////        int num = 0;
+////        for (int i=1;i<100000;i++){
+////            URL url = new URL("http://47.93.242.136:8686/index.php?m=toupiao&c=index&a=add_form&id="+i);
+////            Map<String,String>map = new HashMap<>();
+////            map.put("X-Forwarded-For", IpUtil.getRandomIp());
+////            String ret = getRet(url,map);
+////            if (ret.equals("0"))num++;
+////        }
+//        return 0;
+//    }
     @Override
-    @RequestMapping(value = "/getBrushTicket", method = RequestMethod.GET)
-    public Object getBrushTicket() throws MalformedURLException {
-//        int num = 0;
-//        for (int i=1;i<100000;i++){
-//            URL url = new URL("http://47.93.242.136:8686/index.php?m=toupiao&c=index&a=add_form&id="+i);
-//            Map<String,String>map = new HashMap<>();
-//            map.put("X-Forwarded-For", IpUtil.getRandomIp());
-//            String ret = getRet(url,map);
-//            if (ret.equals("0"))num++;
-//        }
-        return 0;
+    @IgnoreToken
+    @RequestMapping(value = "getWxConfig",method = RequestMethod.GET)
+    public Object getWxConfig(@RequestParam(value = "url") String url) throws MalformedURLException {
+        //获得access_token
+        StringBuffer urlAccStr = new StringBuffer("https://api.weixin.qq.com/cgi-bin/token?grant_type=");
+        urlAccStr.append(grant_type);
+        urlAccStr.append("&appid=");
+        urlAccStr.append(appId);
+        urlAccStr.append("&secret=");
+        urlAccStr.append(secret);
+        URL urlAccUrl = new URL(urlAccStr.toString());
+        String accessToken = getUrlWithObject(urlAccUrl,"access_token");
+
+        //获取jsapi_ticket
+        StringBuffer urlJsapiStr = new StringBuffer("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=");
+        urlJsapiStr.append(accessToken);
+        urlJsapiStr.append("&type=jsapi");
+        URL urlJsapiUrl = new URL(urlJsapiStr.toString());
+        String jsapiTicket = getUrlWithObject(urlJsapiUrl,"ticket");
+
+        Map<String, String> ret = sign(jsapiTicket, url);
+        ret.put("appId", appId);
+        ret.put("jsApiList", appId);
+
+        return ret;
     }
 
     private Map<String, Map<String, String>> getUrlWithMap(URL murl) {
@@ -93,6 +130,15 @@ public class PublicApiImpl implements PublicApi {
             map = JSONUtils.parse(ret, HashMap.class);
         }
         return map;
+    }
+
+    private String getUrlWithObject(URL murl, String param) {
+        String ret = getRet(murl);
+        Map<String, Object> map = null;
+        if (!"".equals(ret)) {
+            map = JSONUtils.parse(ret, HashMap.class);
+        }
+        return (String) map.get(param);
     }
 
     private String getRet(URL url) {
@@ -140,5 +186,62 @@ public class PublicApiImpl implements PublicApi {
             e.printStackTrace();
         }
         return ret;
+    }
+
+    public static Map<String, String> sign(String jsapi_ticket, String url) {
+        Map<String, String> ret = new HashMap<String, String>();
+        String nonce_str = create_nonce_str();
+        String timestamp = create_timestamp();
+        String string1;
+        String signature = "";
+
+        //注意这里参数名必须全部小写，且必须有序
+        string1 = "jsapi_ticket=" + jsapi_ticket +
+                  "&noncestr=" + nonce_str +
+                  "&timestamp=" + timestamp +
+                  "&url=" + url;
+
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(string1.getBytes("UTF-8"));
+            signature = byteToHex(crypt.digest());
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+        ret.put("url", url);
+        ret.put("jsapi_ticket", jsapi_ticket);
+        ret.put("nonceStr", nonce_str);
+        ret.put("timestamp", timestamp);
+        ret.put("signature", signature);
+
+        return ret;
+    }
+
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+    private static String create_nonce_str() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static String create_timestamp() {
+        return Long.toString(System.currentTimeMillis() / 1000);
     }
 }
--- backend/backend/admin/admin-api/src/main/java/com/admin/user/api/PublicApi.java
+++ backend/backend/admin/admin-api/src/main/java/com/admin/user/api/PublicApi.java
@@ -2,14 +2,19 @@ package com.admin.user.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.net.MalformedURLException;
 import java.text.ParseException;
+import java.util.Map;
 
 public interface PublicApi {
     @RequestMapping(value = "/public/getBingDayPic",method = RequestMethod.GET)
     Object getBingDayPic() throws MalformedURLException, ParseException;
 
-    @RequestMapping(value = "/public/getBrushTicket",method = RequestMethod.GET)
-    Object getBrushTicket() throws MalformedURLException;
+//    @RequestMapping(value = "/public/getBrushTicket",method = RequestMethod.GET)
+//    Object getBrushTicket() throws MalformedURLException;
+
+    @RequestMapping(value = "/public/getWxConfig",method = RequestMethod.GET)
+    Object getWxConfig(@RequestParam(value = "url") String url) throws MalformedURLException;
 }
